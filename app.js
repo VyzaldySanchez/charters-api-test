7cdfdfd05f75a94d326dd988a9c474cca622a5d4
Vyzaldy Andres Sanchez Rosario <vyzaldysanchez@gmail.com>
2015-08-06 06:46:26 -0400

    Separates controllers and routes from main file

--- app.js
+++ app.js
@@ -1,97 +1,84 @@
 'use strict';
 
 var Hapi = require('hapi'),
-    Joi = require('joi'),
-    app = new Hapi.Server(),
-    port = process.env.PORT || 3000,
-    pack = require('./package.json'),
-    swaggerOptions = {
-      apiVersion: pack.version,
-      documentationPath: '/documentation',
-      apis:[
-        {
-          path: 'index'
-        }
-      ]
-    };
+  Joi = require('joi'),
+  app = new Hapi.Server(),
+  port = process.env.PORT || 3000,
+  pack = require('./package'),
+  chartersRoutes = require('./lib/routers/charters'),
+  swaggerOptions = {
+    apiVersion: pack.version,
+    documentationPath: '/documentation'
+  };
 
-module.exports = app;
+app.connection({
+  port: port
+});
 
-app.connection({ port: port });
-
-app.route([
-  {
-    method: 'GET',
-    path: '/',
-    config: {
-      handler: function (req, res) {
-        res('Welcome').redirect('/charters');
+app.route([{
+  method: 'GET',
+  path: '/',
+  config: {
+    handler: function(req, res) {
+      res('Welcome').redirect('/charters');
+    }
+  },
+}, {
+  method: 'GET',
+  path: '/charters',
+  config: {
+    handler: chartersRoutes.index,
+    description: 'Show all registered charters',
+    tags: ['api', 'charters']
+  }
+}, {
+  method: 'GET',
+  path: '/charters/{id}',
+  config: {
+    handler: chartersRoutes.getCharter,
+    validate: {
+      params: {
+        id: Joi.number()
+          .required()
+          .description('This is the ID to identify the charter.')
       }
     },
-  },
-  {
-    method: 'GET',
-    path: '/charters',
-    config: {
-      handler: function (req, res) {
-        res(JSON.stringify({ greet: 'Hi' }));
-      },
-      description: 'Show all registered charters',
-      tags: ['api', 'charters']
-    }
-  },
-  {
-    method: 'GET',
-    path: '/lol/{id}',
-    config: {
-      handler: function (req, res) {
-        res(JSON.stringify({ greet: 'Hi' }));
-      },
-      description: 'Show all registered charters',
-      tags: ['api', 'lol'],
-      validate: {
-        params: {
-          id: Joi.number()
-                .required()
-                .description('The example id.')
-        }
-      }
-    }
-  },
-  {
-    method: ['POST'],
-    path: '/lol/{name}',
-    config: {
-      handler: function (req, res) {
-        res(JSON.stringify({ greet: 'Hi' + req.params.name }));
-      },
-      description: 'Save a new charter',
-      tags: ['api', 'lol'],
-      validate: {
-        params: {
-          name: Joi.string()
-                .required()
-                .description('The example name.')
-        }
-      }
-    }
+    tags: ['api', 'charters'],
+    description: 'Get one user\'s charter by his/her ID'
   }
-]);
+}, {
+  method: 'POST',
+  path: '/charters',
+  config: {
+    handler: chartersRoutes.registerCharter,
+    validate: {
+      params: {
+        charter: Joi.string()
+          .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
+          .required()
+          .description('The charter to be saved.')
+      }
+    },
+    tags: ['api', 'charters'],
+    description: 'Register a charter to the docs collection'
+  }
+}]);
 
 
-app.register([
-  {
-    register: require('hapi-swagger'),
-    options: swaggerOptions
-  }
-], function (err) {
+app.register([{
+  //Register Swagger plugin for hapi.js api's documentation
+  register: require('hapi-swagger'),
+  options: swaggerOptions
+}], function(err) {
   if (err) {
     app.log(['error'], 'hapi-swagger failed to load: ' + err);
   } else {
     app.log(['error'], 'hapi-swagger loaded');
 
-    app.start(function () {
+    app.start(function() {
       console.log('App running on port', port);
     });
   }
 });
+
+module.exports = app;
