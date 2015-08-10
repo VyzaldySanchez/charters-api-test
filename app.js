0a08ed40f75e0fea0a3793bf5df25a00a2540f5a
Vyzaldy Andres Sanchez Rosario <vyzaldysanchez@gmail.com>
2015-08-10 11:04:10 -0400

    Fixes some issues in main file and set controllers to handle payloads

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var Hapi = require('hapi'),
   app = new Hapi.Server(),
   port = process.env.PORT || 3000,
   pack = require('./package'),
+  mongoose = require('mongoose'),
   chartersRoutes = require('./lib/routers/charters'),
   swaggerOptions = {
     apiVersion: pack.version,
     documentationPath: '/documentation'
+  },
+  dbUrl = 'mongodb://localhost:27017/test',
+  dbOptions = {
+    db: {
+      native_parser: true
+    },
+    server: {
+      poolSize: 5
+    }
   };
 
 app.connection({
@@ -38,7 +48,7 @@ app.route([{
     handler: chartersRoutes.getCharter,
     validate: {
       params: {
-        id: Joi.number()
+        id: Joi.string()
           .required()
           .description('This is the ID to identify the charter.')
       }
@@ -52,7 +62,7 @@ app.route([{
   config: {
     handler: chartersRoutes.registerCharter,
     validate: {
-      params: {
+      payload: {
         charter: Joi.string()
           .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
           .required()
@@ -62,8 +72,43 @@ app.route([{
     tags: ['api', 'charters'],
     description: 'Register a charter to the docs collection'
   }
+}, {
+  method: 'PUT',
+  path: '/charters/{charterId}/update',
+  config: {
+    handler: chartersRoutes.updateCharter,
+    validate: {
+      payload: {
+        newCharter: Joi.string()
+          .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
+          .required()
+          .description('New charter\'s value.')
+      },
+      params: {
+        charterId: Joi.string()
+          .required()
+          .description('The charter\'s ID that is going to be update'),
+      }
+    },
+    tags: ['api', 'charters'],
+    description: 'Updates a charter based on the ID given.'
+  }
+}, {
+  method: 'DELETE',
+  path: '/charters/{id}/delete',
+  config: {
+    handler: chartersRoutes.deleteCharter,
+    validate: {
+      params: {
+        id: Joi.string()
+          .required()
+          .description('The id to search to delete a charter with this id')
+      }
+    },
+    tags: ['api', 'charters'],
+    description: 'Deletes a charter from the collection'
+  }
 }]);
-
 
 app.register([{
   //Register Swagger plugin for hapi.js api's documentation
@@ -71,14 +116,28 @@ app.register([{
   options: swaggerOptions
 }], function(err) {
   if (err) {
-    app.log(['error'], 'hapi-swagger failed to load: ' + err);
+    app.log(['error'], 'plugins failed to load: ' + err);
   } else {
-    app.log(['error'], 'hapi-swagger loaded');
+    app.log(['error'], 'plugins loaded');
 
-    app.start(function() {
-      console.log('App running on port', port);
+
+    mongoose.connect(dbUrl, dbOptions, function(err) {
+      if (err) app.log('error', err);
+      app.start(function() {
+        console.log('App running on port', port);
+      });
     });
   }
 });
 
-module.exports = app;
+module.exports = {
+  app: function() {
+    return app;
+  },
+  mongoose: function() {
+    return mongoose;
+  },
+  db: function() {
+    return mongoose.connection;
+  }
+};
