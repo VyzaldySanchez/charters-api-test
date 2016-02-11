7a8e6a8cf054e942a2dd05e46a5e962770f56af8
Vyzaldy Andres Sanchez Rosario <vyzaldysanchez@gmail.com>
2016-02-11 00:56:13 -0400

    Beautifies some code

--- app.js
+++ app.js
@@ -11,138 +11,138 @@ var Hapi = require('hapi'),
     apiVersion: pack.version,
     documentationPath: '/documentation'
   },
-  dbUrl =
-  'mongodb://heroku_9c30tsbk:rvanpohs97tmlmr4bhfkn404un@ds033143.mongolab.com:33143/heroku_9c30tsbk',
-  dbOptions = {
-    db: {
-      'native_parser': true
-    },
-    server: {
-      poolSize: 5
-    }
-  };
+    dbUrl =
+    'mongodb://heroku_9c30tsbk:rvanpohs97tmlmr4bhfkn404un@ds033143.mongolab.com:33143/heroku_9c30tsbk',
+    dbOptions = {
+        db: {
+            'native_parser': true
+        },
+        server: {
+            poolSize: 5
+        }
+    };
 
 app.connection({
-  port: port
+    port: port
 });
 
 app.route([{
-  method: 'GET',
-  path: '/',
-  config: {
-    handler: function(req, res) {
-      res('Welcome').redirect('/charters');
+    method: 'GET',
+    path: '/',
+    config: {
+        handler: function(req, res) {
+            res('Welcome').redirect('/charters');
+        }
+    },
+}, {
+    method: 'GET',
+    path: '/charters',
+    config: {
+        handler: chartersRoutes.index,
+        description: 'Show all registered charters',
+        tags: ['api', 'charters']
     }
-  },
 }, {
-  method: 'GET',
-  path: '/charters',
-  config: {
-    handler: chartersRoutes.index,
-    description: 'Show all registered charters',
-    tags: ['api', 'charters']
-  }
+    method: 'GET',
+    path: '/charters/{id}',
+    config: {
+        handler: chartersRoutes.getCharter,
+        validate: {
+            params: {
+                id: Joi.string()
+                    .required()
+                    .description('This is the ID to identify the charter.')
+            }
+        },
+        tags: ['api', 'charters'],
+        description: 'Get one user\'s charter by his/her ID'
+    }
 }, {
-  method: 'GET',
-  path: '/charters/{id}',
-  config: {
-    handler: chartersRoutes.getCharter,
-    validate: {
-      params: {
-        id: Joi.string()
-          .required()
-          .description('This is the ID to identify the charter.')
-      }
-    },
-    tags: ['api', 'charters'],
-    description: 'Get one user\'s charter by his/her ID'
-  }
+    method: 'POST',
+    path: '/charters',
+    config: {
+        handler: chartersRoutes.registerCharter,
+        validate: {
+            payload: {
+                charter: Joi.string()
+                    .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
+                    .required()
+                    .description('The charter to be saved.')
+            }
+        },
+        tags: ['api', 'charters'],
+        description: 'Register a charter to the docs collection'
+    }
 }, {
-  method: 'POST',
-  path: '/charters',
-  config: {
-    handler: chartersRoutes.registerCharter,
-    validate: {
-      payload: {
-        charter: Joi.string()
-          .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
-          .required()
-          .description('The charter to be saved.')
-      }
-    },
-    tags: ['api', 'charters'],
-    description: 'Register a charter to the docs collection'
-  }
+    method: 'PUT',
+    path: '/charters/{charterId}/update',
+    config: {
+        handler: chartersRoutes.updateCharter,
+        validate: {
+            payload: {
+                newCharter: Joi.string()
+                    .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
+                    .required()
+                    .description('New charter\'s value.')
+            },
+            params: {
+                charterId: Joi.string()
+                    .required()
+                    .description('The charter\'s ID that is going to be update'),
+            }
+        },
+        tags: ['api', 'charters'],
+        description: 'Updates a charter based on the ID given.'
+    }
 }, {
-  method: 'PUT',
-  path: '/charters/{charterId}/update',
-  config: {
-    handler: chartersRoutes.updateCharter,
-    validate: {
-      payload: {
-        newCharter: Joi.string()
-          .regex(/^(\d{3})-(\d{7})-(\d{1})$/i)
-          .required()
-          .description('New charter\'s value.')
-      },
-      params: {
-        charterId: Joi.string()
-          .required()
-          .description('The charter\'s ID that is going to be update'),
-      }
-    },
-    tags: ['api', 'charters'],
-    description: 'Updates a charter based on the ID given.'
-  }
-}, {
-  method: 'DELETE',
-  path: '/charters/{id}/delete',
-  config: {
-    handler: chartersRoutes.deleteCharter,
-    validate: {
-      params: {
-        id: Joi.string()
-          .required()
-          .description(
-            'The id to search to delete a charter with this id')
-      }
-    },
-    tags: ['api', 'charters'],
-    description: 'Deletes a charter from the collection'
-  }
+    method: 'DELETE',
+    path: '/charters/{id}/delete',
+    config: {
+        handler: chartersRoutes.deleteCharter,
+        validate: {
+            params: {
+                id: Joi.string()
+                    .required()
+                    .description(
+                        'The id to search to delete a charter with this id')
+            }
+        },
+        tags: ['api', 'charters'],
+        description: 'Deletes a charter from the collection'
+    }
 }]);
 
 app.register([{
-  //Register Swagger plugin for hapi.js api's documentation
-  register: require('hapi-swagger'),
-  options: swaggerOptions
-}], function(err) {
-  if (err) {
-    app.log(['error'], 'plugins failed to load: ' + err);
-  } else {
+    //Register Swagger plugin for hapi.js api's documentation
+    register: require('hapi-swagger'),
+    options: swaggerOptions
+}],
+function(err) {
+    if (err) {
+        app.log(['error'], 'plugins failed to load: ' + err);
+        return;
+    }
     app.log(['error'], 'plugins loaded');
 
-
     mongoose.connect(dbUrl, dbOptions, function(err) {
-      if (err) {
-        app.log('error', err);
-      }
+        if (err) {
+            app.log('error', err);
+        }
 
-      app.start(function() {
-        console.log('App running on port', port);
-      });
+        app.start(function() {
+            console.log('App running on port', port);
+        });
     });
-  }
 });
 
 module.exports = {
-  app: function() {
-    return app;
-  },
-  mongoose: function() {
-    return mongoose;
-  },
-  db: function() {
-    return mongoose.connection;
-  }
+    app: function() {
+        return app;
+    },
+    mongoose: function() {
+        return mongoose;
+    },
+    db: function() {
+        return mongoose.connection;
+    }
 };
